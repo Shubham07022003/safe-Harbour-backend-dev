8abc811028fc8b7df0e7754b6c698924472774b7
divyeshmoraniya <hdivyesh62@gmail.com>
2025-12-18 16:16:13 +0530

    create a forgot password api for the counsellor

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -18,7 +18,7 @@
 
 import { FormController } from "./Form.controllers.js";
 import { SignUp ,Login , amdinLogin, sendEmailOtp , VerifyOtp ,getHistory } from "./User.controllers.js";
-import { CounsellorSignup , CounsellorLogin , getallCounsellor, getCounsellorByEmail } from "./Counsellor.controllers.js";
+import { CounsellorSignup , CounsellorLogin , getallCounsellor, getCounsellorByEmail,resetPassword } from "./Counsellor.controllers.js";
 
 // Export all controllers together
 export {
@@ -32,5 +32,6 @@ export {
     CounsellorLogin,
     CounsellorSignup,
     getallCounsellor,
-    getCounsellorByEmail
+    getCounsellorByEmail,
+    resetPassword
 };
